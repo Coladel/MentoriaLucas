b6505c404b96a65cf617eeb0c992c5af743fcff9
lucas renan <contato@lucasrenan.com>
2016-08-25 00:21:20 -0300

    Add create method

--- Create.rb
+++ Create.rb
@@ -1,9 +1,9 @@
-puts "Digite sua mensagem!"
-	mensagem = gets.chomp	
-
 require 'pg'
-conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
-res = conn.exec("INSERT INTO posts(post) VALUES('#{mensagem}')")
 
-res.clear
-conn.close
+def create(value)
+  conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
+  res = conn.exec("INSERT INTO posts(post) VALUES('#{value}')")
+
+  res.clear
+  conn.close
+end
