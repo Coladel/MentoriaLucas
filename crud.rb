181afd18f627e2c87826bde9aa5144bf3f1c423d
lucas renan <contato@lucasrenan.com>
2016-08-28 16:58:04 -0300

    Add create method (#2)

--- crud.rb
+++ crud.rb
@@ -1,28 +1,32 @@
-puts "CRUD do João, Gerencie aqui seus posts"
+require './create'
 
-puts "Digite o número referente a opção que deseja utilizar!"
+puts 'CRUD do João, Gerencie aqui seus posts'
+
+puts 'Digite o número referente a opção que deseja utilizar!'
 puts
-puts "1 - Criar Post"
-puts "2 - Ler Post"
-puts "3 - Alterar Post"
-puts "4 - Deletar Post"
+puts '1 - Criar Post'
+puts '2 - Ler Post'
+puts '3 - Alterar Post'
+puts '4 - Deletar Post'
 
+puts 'Informe a opção:'
+action = gets.chomp
 
-puts "Informe a opção:"
-opcao = gets.chomp
+if action == '1'
+  puts 'Digite sua mensagem!'
+  message = gets.chomp
 
-if opcao == '1'
-	load "create.rb"
+  create(message)
 end
 
-if opcao == '2'
-	load "read.rb"
+if action == '2'
+  load 'read.rb'
 end
 
-if opcao =='3'
-	load 'update.rb'
+if action =='3'
+  load 'update.rb'
 end
 
-if opcao =='4'
-	load 'delete.rb'
+if action =='4'
+  load 'delete.rb'
 end
