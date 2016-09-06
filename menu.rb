08379e57c530f6f6436d323bc01174f13e0942cd
Coladel <coladel@gmail.com>
2016-09-05 21:33:50 -0300

    menu_in_a_separeted_file

---
+++ menu.rb
@@ -0,0 +1,13 @@
+def menu
+    puts
+    puts 'CRUD do João, Gerencie aqui seus posts'
+    puts
+    puts 'Digite o número referente a opção que deseja utilizar!'
+    puts
+    puts '1 - Criar Post'
+    puts '2 - Ler Post'
+    puts '3 - Alterar Post'
+    puts '4 - Deletar Post'
+    puts
+    puts 'Informe a opção:'
+end
