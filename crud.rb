f66284693ffbe745148b56fbd34c0638ce0fa9f9
Coladel <coladel@gmail.com>
2016-09-05 21:37:49 -0300

    crud_call_menu_and_action_correction

--- crud.rb
+++ crud.rb
@@ -2,18 +2,10 @@ require './create'
 require './read'
 require './delete'
 require './update'
+require './menu'
 
-puts
-puts 'CRUD do João, Gerencie aqui seus posts'
-puts
-puts 'Digite o número referente a opção que deseja utilizar!'
-puts
-puts '1 - Criar Post'
-puts '2 - Ler Post'
-puts '3 - Alterar Post'
-puts '4 - Deletar Post'
+menu
 
-puts 'Informe a opção:'
 action = gets.chomp
 
 if action == '1'
