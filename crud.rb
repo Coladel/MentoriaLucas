40b2b8c3e4be0f9f51ad1c87c6bb14d37c7412d6
lucas renan <contato@lucasrenan.com>
2016-09-06 17:35:05 -0300

    Fix identation issues and remove unused code

--- crud.rb
+++ crud.rb
@@ -5,43 +5,46 @@ require './update'
 require './menu'
 
 menu
-
 action = gets.chomp
 
 if action == '1'
-    puts 'Digite sua mensagem!'
-    message = gets.chomp
-    create(message)
-    puts "Veja sua mensagem postada"
-    read_last_one
+  puts 'Digite sua mensagem!'
+  message = gets.chomp
+  create(message)
+  puts 'Veja sua mensagem postada'
+  read_last_one
 end
 
 if action == '2'
-    puts "Digite o Id referente qual mensagem deseja ler"
-    puts "Para ler todas as mensagens digite all"
-    id_message = gets.chomp
-    if id_message  == 'all'
-      read_all
-    else
-      read_individual(id_message)
-    end
+  puts 'Digite o Id referente qual mensagem deseja ler'
+  puts 'Para ler todas as mensagens digite all'
+  id_message = gets.chomp
+
+  if id_message  == 'all'
+    read_all
+  else
+    read_individual(id_message)
+  end
 end
 
-if action =='3'
-  puts "Informe o id da mensagem que deseja atualizar"
+if action == '3'
+  puts 'Informe o id da mensagem que deseja atualizar'
   id_message = gets.chomp
   read_individual(id_message)
-  puts "Digite a nova mensagem para o Post:"
+
+  puts 'Digite a nova mensagem para o Post:'
   message_update = gets.chomp
   update_choice_post(message_update, id_message)
-  puts "Confira sua mensagem atualizada!"
+
+  puts 'Confira sua mensagem atualizada!'
   read_individual(id_message)
 end
 
-if action =='4'
-  puts "Digite o Id da mensagem que deseja deletar"
+if action == '4'
+  puts 'Digite o Id da mensagem que deseja deletar'
   id_message = gets.chomp
   read_individual(id_message)
+
   delete_post(id_message)
-  puts "Esta mensagem foi deletada!"
+  puts 'Esta mensagem foi deletada!'
 end
--- read.rb
+++ read.rb
@@ -2,32 +2,35 @@ require 'pg'
 
 def read_individual(value)
   conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
-  res = conn.exec("select post from posts where id = #{value}")
+  res = conn.exec("SELECT post FROM posts WHERE id = #{value}")
   res.each do |r|
     puts r
   end
-res.clear
-conn.close
+
+  res.clear
+  conn.close
 end
 
 
 def read_all
   conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
-  res = conn.exec("select * from posts")
+  res = conn.exec('SELECT * FROM posts')
   res.each do |r|
     puts r
   end
-res.clear
-conn.close
+
+  res.clear
+  conn.close
 end
 
 def read_last_one
   conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
-  res = conn.exec("select * from posts ORDER BY id desc limit 1")
+  res = conn.exec('SELECT * FROM posts ORDER BY id DESC LIMIT 1')
   res.each do |r|
     puts r
   end
-res.clear
-conn.close
+
+  res.clear
+  conn.close
 end
 
