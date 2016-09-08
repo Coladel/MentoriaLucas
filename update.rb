54846fce1441974b1e99e8fd56340ecbd0275e05
Coladel <coladel@gmail.com>
2016-09-07 21:54:36 -0300

    update_with_connection_method

--- update.rb
+++ update.rb
@@ -1,8 +1,5 @@
-require 'pg'
+require './connectbd'
 
 def update_choice_post(value, message_id)
-  conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
-  res = conn.exec("UPDATE posts SET post = '#{value}' WHERE id = '#{message_id}'")
-  res.clear
-  conn.close
+  connect_bd_and_exec("UPDATE posts SET post = '#{value}' WHERE id = '#{message_id}'")
 end
