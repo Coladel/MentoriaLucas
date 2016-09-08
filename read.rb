9ad9ce1444c00bed63d6879e0aaa96c7105f6f91
Coladel <coladel@gmail.com>
2016-09-07 21:53:56 -0300

    read_with_connection_method

--- read.rb
+++ read.rb
@@ -1,36 +1,13 @@
-require 'pg'
+require './connectbd'
 
 def read_individual(value)
-  conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
-  res = conn.exec("SELECT post FROM posts WHERE id = #{value}")
-  res.each do |r|
-    puts r
-  end
-
-  res.clear
-  conn.close
+  connect_bd_and_exec("SELECT post FROM posts WHERE id = #{value}")
 end
 
-
 def read_all
-  conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
-  res = conn.exec('SELECT * FROM posts')
-  res.each do |r|
-    puts r
-  end
-
-  res.clear
-  conn.close
+  connect_bd_and_exec('SELECT * FROM posts')
 end
 
 def read_last_one
-  conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
-  res = conn.exec('SELECT * FROM posts ORDER BY id DESC LIMIT 1')
-  res.each do |r|
-    puts r
-  end
-
-  res.clear
-  conn.close
+  connect_bd_and_exec('SELECT * FROM posts ORDER BY id DESC LIMIT 1')
 end
-
