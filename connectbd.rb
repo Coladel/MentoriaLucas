3fc856e8cd64b92b14ba14f7c8d18681151e7fa1
Coladel <coladel@gmail.com>
2016-09-07 21:51:26 -0300

    connect_and_exec_method_correction1

--- connectbd.rb
+++ connectbd.rb
@@ -1,8 +1,12 @@
 require 'pg'
 
 def connect_bd_and_exec(sql_command)
-    conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
-    res = conn.exec("#{sql_command}")
-    res.clear
-    conn.close
+  conn = PGconn.connect('localhost', 5432, nil, nil, 'posts', 'postgres', 'mude6363')
+  res = conn.exec("#{sql_command}")
+  res.each do |r|
+    puts r
+  end
+
+  res.clear
+  conn.close
 end
