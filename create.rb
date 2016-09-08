bd827038fe84e2af643f5161c186d030e442e45b
Coladel <coladel@gmail.com>
2016-09-07 21:52:24 -0300

    create_with_connection_method_correction1

--- create.rb
+++ create.rb
@@ -1,5 +1,5 @@
 require './connectbd'
 
 def create(value)
-    connect_bd_and_exec("INSERT INTO posts(post) VALUES('#{value}')")
+  connect_bd_and_exec("INSERT INTO posts(post) VALUES('#{value}')")
 end
