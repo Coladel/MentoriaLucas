3203cfb27b72732d918d8e63188954f7cebf9af2
Coladel <coladel@gmail.com>
2016-09-11 18:11:48 -0300

    class_post_included

---
+++ post.rb
@@ -0,0 +1,22 @@
+require './connectbd'
+
+class Post
+  def self.create(value)
+  connect_bd_and_exec("INSERT INTO posts(post) VALUES('#{value}')")
+  end
+  def self.delete(value)
+  connect_bd_and_exec("DELETE FROM posts WHERE id = '#{value}'")
+  end
+  def self.update(value, message_id)
+  connect_bd_and_exec("UPDATE posts SET post = '#{value}' WHERE id = '#{message_id}'")
+  end
+  def self.read_individual(value)
+  connect_bd_and_exec("SELECT post FROM posts WHERE id = #{value}")
+  end
+  def self.read_all
+  connect_bd_and_exec('SELECT * FROM posts')
+  end
+  def self.read_last_one
+  connect_bd_and_exec('SELECT * FROM posts ORDER BY id DESC LIMIT 1')
+  end
+end
--- update.rb
+++
@@ -1,5 +0,0 @@
-require './connectbd'
-
-def update_choice_post(value, message_id)
-  connect_bd_and_exec("UPDATE posts SET post = '#{value}' WHERE id = '#{message_id}'")
-end
