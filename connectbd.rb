ce7cc85336acabf86589c0a28b55cf0be93c3706
Coladel <coladel@gmail.com>
2016-09-19 22:11:41 -0300

    correction_env_updatename

--- connectbd.rb
+++ connectbd.rb
@@ -3,7 +3,7 @@ require 'dotenv'
 Dotenv.load
 
 def connect_bd_and_exec(sql_command)
-  conn = PGconn.connect(ENV['DB_HOST'], ENV['DB_PORT'], nil, nil, ENV['DB_NAME'], ENV['DB_USER'], ENV['DB_SECRET_KEY'])
+  conn = PGconn.connect(ENV['DB_HOST'], ENV['DB_PORT'], nil, nil, ENV['DB_NAME'], ENV['DB_USER'], ENV['DB_PASSWORD'])
   res = conn.exec("#{sql_command}")
   res.each do |r|
     puts r
