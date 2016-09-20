e86ce0146297d4669dc6fccf9e32725cb69bba07
Coladel <coladel@gmail.com>
2016-09-19 21:42:04 -0300

    include_db_port_and_load_corrected

--- connectbd.rb
+++ connectbd.rb
@@ -1,9 +1,9 @@
 require 'pg'
 require 'dotenv'
-Dotenv.load('keys.env')
+Dotenv.load
 
 def connect_bd_and_exec(sql_command)
-  conn = PGconn.connect(ENV['DB_HOST'], 5432, nil, nil, ENV['DB_NAME'], ENV['DB_USER'], ENV['DB_SECRET_KEY'])
+  conn = PGconn.connect(ENV['DB_HOST'], ENV['DB_PORT'], nil, nil, ENV['DB_NAME'], ENV['DB_USER'], ENV['DB_SECRET_KEY'])
   res = conn.exec("#{sql_command}")
   res.each do |r|
     puts r
--- delete.rb
+++
@@ -1,5 +0,0 @@
-require './connectbd'
-
-def delete_post(value)
-  connect_bd_and_exec("DELETE FROM posts WHERE id = '#{value}'")
-end
